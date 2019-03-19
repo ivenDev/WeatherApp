289cd3cec08b4e87b0f8405191447609d7241505
Cloniamix Home <cloniamix@mail.ru>
2019-03-19 19:17:26 +0300

    add:   - item_city_weather

--- src/main/java/com/example/cloniamix/weatherapp/MainActivity.java
+++ src/main/java/com/example/cloniamix/weatherapp/MainActivity.java
@@ -13,3 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+// TODO: 19.03.2019 Добавить необходимые библиотеки для работы с RecyclerView
+// TODO: 19.03.2019 В activity_main добавить RecyclerView
+// TODO: 19.03.2019 Добавить RecyclerView в Main Activity (адаптер и т.п.)
+// TODO: 19.03.2019 В item_city_weather применить единый стиль и посмотреть какие значения рекомендует material design

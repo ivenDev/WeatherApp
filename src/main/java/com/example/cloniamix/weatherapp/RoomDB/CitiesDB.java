5b1fcb5ca97e840136c2d8a10de88433cc6102e2
CloniamixVKM <cloniamix@yandex.ru>
2019-03-29 00:20:37 +0300

    Промежуточный коммит

--- src/main/java/com/example/cloniamix/weatherapp/RoomDB/CitiesDB.java
+++ src/main/java/com/example/cloniamix/weatherapp/RoomDB/CitiesDB.java
@@ -1,12 +1,18 @@
 package com.example.cloniamix.weatherapp.RoomDB;
 
+import android.util.Log;
+
 import com.example.cloniamix.weatherapp.RoomDB.Dao.CitiesDao;
 import com.example.cloniamix.weatherapp.RoomDB.Entity.City;
+import com.example.cloniamix.weatherapp.app.Const;
 import com.example.cloniamix.weatherapp.app.MyApp;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 
 
 // TODO: 20.03.2019 Реализовать миграцию версий БД
@@ -26,8 +32,21 @@ public abstract class CitiesDB extends RoomDatabase {
                             CitiesDB.class, "CityWeather.db")
                             //.allowMainThreadQueries() //исключает ошибку, если работать с БД в основном потоке
                             .build();
-                    INSTANCE.citiesDao().insertCity(new City("Saransk"));
-                    INSTANCE.citiesDao().insertCity(new City("Moscow"));
+                    CompositeDisposable disposable = new CompositeDisposable();
+                    disposable.add(INSTANCE.citiesDao().insertCity(new City("Saransk"))
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(()-> Log.d(Const.APP_TAG, "initDB: Город: Saransk добавлен")
+                                    ,throwable -> Log.d(Const.APP_TAG, "initDB: Ошибка добавления"))
+                    );
+                    disposable.add(INSTANCE.citiesDao().insertCity(new City("Moscow"))
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(()-> Log.d(Const.APP_TAG, "initDB: Город: Moscow добавлен")
+                                    ,throwable -> Log.d(Const.APP_TAG, "initDB: Ошибка добавления"))
+                    );
+
+                    disposable.dispose();
 
             }
         }

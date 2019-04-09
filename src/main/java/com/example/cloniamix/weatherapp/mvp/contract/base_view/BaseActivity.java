043a2148b808ae2fa71454290580cdaec9ab12ea
CloniamixVKM <cloniamix@yandex.ru>
2019-04-09 11:56:05 +0300

    промежуточный

--- src/main/java/com/example/cloniamix/weatherapp/mvp/contract/base_view/BaseActivity.java
+++ src/main/java/com/example/cloniamix/weatherapp/mvp/contract/base_view/BaseActivity.java
@@ -1,10 +1,36 @@
 package com.example.cloniamix.weatherapp.mvp.contract.base_view;
 
+import android.os.Bundle;
+
+import com.example.cloniamix.weatherapp.mvp.contract.base_presenter.BasePresenter;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 // FIXME: 08.04.2019 реализовать базовую активити и унаследовать от нее все активити
-public abstract class BaseActivity extends AppCompatActivity  {
+public abstract class BaseActivity<V extends IBaseView,P extends BasePresenter<V>> extends AppCompatActivity/* implements IBaseView*/  {
 
+    public P mPresenter;
+    V view;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPresenter.subscribe(view);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPresenter.unsubscribe();
+        mPresenter = null;
+    }
 
     public abstract void init();
 }
